41b764b74039547abe9b1794fe93486ea8b9fb9f
Atticus Rex <atticusdrex@gmail.com>
2025-12-14 19:46:42 -0500

    Changing Laminar Flame Speed Plot

--- LaminarFlameSpeed/plotter.py
+++ LaminarFlameSpeed/plotter.py
@@ -25,8 +25,8 @@ if __name__ == "__main__":
     with open("models/kr.pkl", "rb") as infile:
         kr = pickle.load(infile)   
     # Setting up the plot parameters 
-    plt.figure(figsize=(10,8),dpi = 300)
-    plt.subplot(2,2,1)
+    fig, axs = plt.subplots(4,4,figsize=(10,10),dpi = 300)
+    # plt.subplot(4,4,1)
 
     # Iterating through the test temps 
     test_temps = [550, 650, 750, 850]
@@ -111,38 +111,91 @@ if __name__ == "__main__":
         kr_conf = 1.96 * np.sqrt(kr_cov)
 
         # Plotting the data 
-        plt.subplot(2,2,plot_num+1)
+        ax = plt.subplot(4,4,4*plot_num+1)
         # Plotting the Hyperkriging predictions with uncertainty estimates
         plt.plot(scaler.inverse_transform(Xtest)[:,0], hk_mean, color = 'green', label = 'Proposed Method')
         plt.fill_between(scaler.inverse_transform(Xtest)[:,0], hk_mean-hk_conf, hk_mean + hk_conf, color = 'green', alpha = 0.3)
-
-        # Plotting the AR1 predictions with uncertainty estimates
-        plt.plot(scaler.inverse_transform(Xtest)[:,0], koh_mean, color = 'red', label = 'Kennedy O\'Hagan')
-        plt.fill_between(scaler.inverse_transform(Xtest)[:,0], koh_mean-koh_conf, koh_mean + koh_conf, color = 'red', alpha = 0.3)
-
-        # Plotting the NARGP predictions with uncertainty estimates
-        plt.plot(scaler.inverse_transform(Xtest)[:,0], nargp_mean, color = 'orange', label = 'NARGP')
-        plt.fill_between(scaler.inverse_transform(Xtest)[:,0], nargp_mean-nargp_conf, nargp_mean + nargp_conf, color = 'orange', alpha = 0.3)
-
-        # Plotting the NARGP predictions with uncertainty estimates
-        plt.plot(scaler.inverse_transform(Xtest)[:,0], kr_mean, color = 'blue', label = 'Kriging')
-        plt.fill_between(scaler.inverse_transform(Xtest)[:,0], kr_mean-kr_conf, kr_mean + kr_conf, color = 'blue', alpha = 0.3)
-
         # Plotting the Fidelity-3 training data for comparison 
         inds = (scaler.inverse_transform(data_dict[3]['X'])[:,1] == test_temp) 
         Xsim, Ysim = data_dict[3]['X'][inds,:], data_dict[3]['Y'][inds]
         plt.scatter(scaler.inverse_transform(Xsim)[:,0], (Ysim), marker = '.', color = 'black', label = 'Lu 206-Step Mechanism')
-
         # Plotting the unseen high-fidelity testing data 
         plt.scatter(Xtrue[:,0], Ytrue, marker = '+', color = 'red', label = "High-Fidelity Testing Data")
+        if plot_num + 1 == 1:
+            plt.title("Proposed Method")
+        plt.ylabel("$T_0=%dK$" % (test_temp), fontsize=14)
+        if plot_num + 1 < 4:
+            ax.set_xticks([])         # Removes all tick marks
+            ax.set_xlabel('')         # Removes the axis label
+
+        ax = plt.subplot(4,4,4*plot_num+2)
+        # Plotting the AR1 predictions with uncertainty estimates
+        plt.plot(scaler.inverse_transform(Xtest)[:,0], koh_mean, color = 'red', label = 'Kennedy O\'Hagan')
+        plt.fill_between(scaler.inverse_transform(Xtest)[:,0], koh_mean-koh_conf, koh_mean + koh_conf, color = 'red', alpha = 0.3)
+        # Plotting the Fidelity-3 training data for comparison 
+        inds = (scaler.inverse_transform(data_dict[3]['X'])[:,1] == test_temp) 
+        Xsim, Ysim = data_dict[3]['X'][inds,:], data_dict[3]['Y'][inds]
+        plt.scatter(scaler.inverse_transform(Xsim)[:,0], (Ysim), marker = '.', color = 'black', label = 'Lu 206-Step Mechanism')
+        # Plotting the unseen high-fidelity testing data 
+        plt.scatter(Xtrue[:,0], Ytrue, marker = '+', color = 'red', label = "High-Fidelity Testing Data")
+        if plot_num + 1 == 1:
+            plt.title("Kennedy O'Hagan")
+        if plot_num + 1 < 4:
+            ax.set_xticks([])         # Removes all tick marks
+            ax.set_xlabel('')         # Removes the axis label
+        ax.set_yticks([])         # Removes all tick marks
+        ax.set_ylabel('')         # Removes the axis label
+        
+        ax = plt.subplot(4,4,4*plot_num+3)
+        # Plotting the NARGP predictions with uncertainty estimates
+        plt.plot(scaler.inverse_transform(Xtest)[:,0], nargp_mean, color = 'orange', label = 'NARGP')
+        plt.fill_between(scaler.inverse_transform(Xtest)[:,0], nargp_mean-nargp_conf, nargp_mean + nargp_conf, color = 'orange', alpha = 0.3)
+        # Plotting the Fidelity-3 training data for comparison 
+        inds = (scaler.inverse_transform(data_dict[3]['X'])[:,1] == test_temp) 
+        Xsim, Ysim = data_dict[3]['X'][inds,:], data_dict[3]['Y'][inds]
+        plt.scatter(scaler.inverse_transform(Xsim)[:,0], (Ysim), marker = '.', color = 'black', label = 'Lu 206-Step Mechanism')
+        # Plotting the unseen high-fidelity testing data 
+        plt.scatter(Xtrue[:,0], Ytrue, marker = '+', color = 'red', label = "High-Fidelity Testing Data")
+        if plot_num + 1 == 1:
+            plt.title("NARGP")
+        if plot_num + 1 < 4:
+            ax.set_xticks([])         # Removes all tick marks
+            ax.set_xlabel('')         # Removes the axis label
+        ax.set_yticks([])         # Removes all tick marks
+        ax.set_ylabel('')         # Removes the axis label
+
+        ax = plt.subplot(4,4,4*plot_num+4)
+        # Plotting the NARGP predictions with uncertainty estimates
+        plt.plot(scaler.inverse_transform(Xtest)[:,0], kr_mean, color = 'blue', label = 'Kriging')
+        plt.fill_between(scaler.inverse_transform(Xtest)[:,0], kr_mean-kr_conf, kr_mean + kr_conf, color = 'lightblue', alpha = 0.3)
+        # Plotting the Fidelity-3 training data for comparison 
+        inds = (scaler.inverse_transform(data_dict[3]['X'])[:,1] == test_temp) 
+        Xsim, Ysim = data_dict[3]['X'][inds,:], data_dict[3]['Y'][inds]
+        plt.scatter(scaler.inverse_transform(Xsim)[:,0], (Ysim), marker = '.', color = 'black', label = 'Lu 206-Step Mechanism')
+        # Plotting the unseen high-fidelity testing data 
+        plt.scatter(Xtrue[:,0], Ytrue, marker = '+', color = 'red', label = "High-Fidelity Testing Data")
+        if plot_num + 1 == 1:
+            plt.title("Kriging")
+        if plot_num + 1 < 4:
+            ax.set_xticks([])         # Removes all tick marks
+            ax.set_xlabel('')         # Removes the axis label
+        ax.set_yticks([])         # Removes all tick marks
+        ax.set_ylabel('')         # Removes the axis label
+
+        # # Plotting the Fidelity-3 training data for comparison 
+        # inds = (scaler.inverse_transform(data_dict[3]['X'])[:,1] == test_temp) 
+        # Xsim, Ysim = data_dict[3]['X'][inds,:], data_dict[3]['Y'][inds]
+        # plt.scatter(scaler.inverse_transform(Xsim)[:,0], (Ysim), marker = '.', color = 'black', label = 'Lu 206-Step Mechanism')
+        # # Plotting the unseen high-fidelity testing data 
+        # plt.scatter(Xtrue[:,0], Ytrue, marker = '+', color = 'red', label = "High-Fidelity Testing Data")
         # Plot labeling 
-        plt.title("Predictions at %dK" % (test_temp))
-        if plot_num > 1:
-            plt.xlabel("Equivalence Ratio, $\phi$")
-        if plot_num == 0 or plot_num == 2:
-            plt.ylabel("Log Laminar Flame Speed - log(m/s)")
-        if plot_num == 0:
-            plt.legend()
+        # plt.title("Predictions at %dK" % (test_temp))
+        # if plot_num > 1:
+        #     plt.xlabel("Equivalence Ratio, $\phi$")
+        # if plot_num == 0 or plot_num == 2:
+        #     plt.ylabel("Log Laminar Flame Speed - log(m/s)")
+        # if plot_num == 0:
+        #     plt.legend()
 
         # Fitting a degree-three polynomial through the testing points so we have a more dense error metric
         features = PolynomialFeatures(degree=3)
@@ -160,6 +213,35 @@ if __name__ == "__main__":
         print("NARGP:           %.3e &  %.4f &  %.4f \\\\" % (np.sqrt(np.mean(nargp_cov + (Yhat - nargp_mean)**2)), np.corrcoef(Yhat.ravel(), nargp_mean.ravel())[0,1], -neg_mll(nargp.d[4]['model'], nargp.d[4]['model'].p)))
         print("Kriging:         %.3e &  %.4f &  %.4f \\\\" % (np.sqrt(np.mean(kr_cov + (Yhat - kr_mean)**2)), np.corrcoef(Yhat.ravel(), kr_mean.ravel())[0,1], -neg_mll(kr, kr.p)))
     
+    fig.supylabel("Log Laminar Flame Speed - log(m/s)", fontsize=16)
+    fig.suptitle("Laminar Flame Speed Predictions at Various Temperatures", fontsize=18)
+    fig.supxlabel("Equivalence Ratio, $\phi$", fontsize=16, ha='center', va = 'bottom', x=0.51, y = 0.08)
+    all_handles = []
+    all_labels = []
+    # Loop through every axis in the flattened 4x4 array
+    for ax in axs.flatten():
+        handles, labels = ax.get_legend_handles_labels()
+        all_handles.extend(handles)
+        all_labels.extend(labels)
+
+    # Clean up duplicates (optional but often useful)
+    # Use a dictionary to maintain order and uniqueness
+    unique_handles_labels = dict(zip(all_labels, all_handles))
+    final_handles = unique_handles_labels.values()
+    final_labels = unique_handles_labels.keys()
+    inds = [2,1,0,3,4,5]
+    final_handles = [list(final_handles)[i] for i in inds]
+    final_labels = [list(final_labels)[i] for i in inds]
+    fig.tight_layout(rect=[0, 0.07, 1, 0.99])
+    fig.legend(
+        final_handles, 
+        final_labels,
+        loc = 'upper center',
+        bbox_to_anchor = (0.5, 0.08),
+        ncol = 3,
+        fontsize = 12
+    )
+
     plt.savefig("results/composite.png")
 
 
@@ -182,7 +264,7 @@ if __name__ == "__main__":
     ax3d.set_xlabel('Equivalence Ratio, $\phi$')
     ax3d.set_ylabel('Temperature, $T_0$')
     ax3d.set_zlabel('LFS (m/s)', rotation='vertical')
-    ax3d.set_title('    LFS vs. Equivalence Ratio & Temperature')
+    ax3d.set_title('Laminar Flame Speed vs. \n Equivalence Ratio & Temperature')
 
     plt.subplots_adjust(wspace=0.00, hspace=0.55, right=0.95)
     box = ax3d.get_position()
@@ -194,7 +276,7 @@ if __name__ == "__main__":
     ax2.scatter(Xvis[:, 0], jnp.exp(Yvis).ravel(), c='red', marker='+', label='Testing Data')
     ax2.set_xlabel('Equivalence Ratio, $\phi$')
     ax2.set_ylabel('LFS (m/s)')
-    ax2.set_title('LFS vs. Equivalence Ratio')
+    ax2.set_title('Laminar Flame Speed vs. Equivalence Ratio')
 
     # --- 2D Marginal Plot 2 (Bottom Right: subplot 4) ---
     ax4 = fig.add_subplot(2, 2, 4)
@@ -202,7 +284,7 @@ if __name__ == "__main__":
     ax4.scatter(Xvis[:, 1], jnp.exp(Yvis).ravel(), c='red', marker='+', label='Testing Data')
     ax4.set_xlabel('Temperature, $T_0$')
     ax4.set_ylabel('LFS (m/s)')
-    ax4.set_title('LFS vs. Temperature')
+    ax4.set_title('Laminar Flame Speed vs. Temperature')
     ax4.legend(loc='upper left', fontsize=10)
 
     # plt.tight_layout()
